c5608d13c8b9c4e3ec0f3eb7a66d519e116c91f8
Roman Chesanov <roman.chesanov>
2022-03-02 12:01:03 +0300

    Initial commit

---
+++ src/test/java/guru/qa/SimpleTests.java
@@ -0,0 +1,12 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SimpleTests {
+    @Test
+    void simpleTest() {
+        assertTrue(true);
+    }
+}
