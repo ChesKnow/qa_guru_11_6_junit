6bf53066539119dfc78f78b72e6a2b4dd718e8cb
ChesKnow <Roman.Chesanov@outlook.com>
2022-04-15 23:21:49 +0300

    ParametrMailTrackingTest for MaraDol

--- src/test/java/guru/qa/ParametrMailTrackingTest.java
+++ src/test/java/guru/qa/ParametrMailTrackingTest.java
@@ -1,32 +1,40 @@
 package guru.qa;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.openqa.selenium.Keys;
-
-import java.awt.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Stream;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 @DisplayName("Сайт Почты России")
 public class ParametrMailTrackingTest {
 
+    @BeforeAll
+    static void startPage() {
+
+        Configuration.browserSize = "1920x1080";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
 
     @AfterEach
-    void closeBrowser() {
-        Selenide.closeWebDriver();
+    void addAttachment() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
     }
 
     @ValueSource(strings = {"RF776045417SG", "RF775860395SG", "RF775827050SG"})
@@ -34,50 +42,8 @@ public class ParametrMailTrackingTest {
     @DisplayName("Проверка отображения трекинга почтовых отправлений")
     void commonSearchTest(String testData) {
         Selenide.open("https://www.pochta.ru/");
-        Selenide.$("#barcode").setValue(testData).pressEnter();
+        Selenide.$("#barcode").scrollTo().setValue(testData).pressEnter();
         Selenide.$$("#page-tracking").find(text(testData)).shouldBe(visible);
     }
 
-    @CsvSource(value = {
-            "г Москва|обл Калининградская, г Калининград|2|2-4 дня",
-            "г Москва|край Приморский, г Владивосток|2|8-10 дней"
-    },
-            delimiterString = "|")
-    @ParameterizedTest(name = "посылки обыкновенной \n" +
-            "   весом 2 кг из Москвы в {0}")
-    @DisplayName("Проверка сроков доставки на сайте")
-    void complexSearchTest(String addressFrom, String addressTo, String weight, String expectedDeliveryDate) {
-        Selenide.open("https://www.pochta.ru/parcels/");
-        Selenide.$("input[name=AddressFrom]").sendKeys("" + Keys.LEFT_ALT+Keys.ENTER+Keys.DELETE);
-        Selenide.$("input[name=AddressFrom]").sendKeys(addressFrom);
-        Selenide.$(withText(addressFrom)).click();
-        ;
-        Selenide.$("input[name=AddressTo]").setValue(addressTo);
-        Selenide.$(withText(addressTo)).click();
-        Selenide.$("#Weight").sendKeys(String.valueOf(weight)+Keys.ENTER);
-        Selenide.$("#parcel-summary").find(withText(expectedDeliveryDate));
-
-    }
-
-    static Stream<Arguments> mixedArgumentsTest() {
-        return Stream.of(
-                Arguments.of("г Москва","обл Калининградская, г Калининград",2, "2-4 дня"),
-                Arguments.of("г Москва","край Приморский, г Владивосток",2,"8-10 дней")
-        );
-    }
-    @Disabled
-    @MethodSource(value = "mixedArgumentsTest")
-    @ParameterizedTest(name = "{3}")
-    @DisplayName("Проверка сроков доставки на сайте")
-    void mixedArgumentTest(String addressFrom, String addressTo, int weight, String expectedDeliveryDate) {
-
-        Selenide.$("input[name=AddressFrom]").sendKeys("" + Keys.LEFT_ALT+Keys.ENTER+Keys.DELETE);
-        Selenide.$("input[name=AddressFrom]").sendKeys(addressFrom);
-        Selenide.$(withText(addressFrom)).click();
-        ;
-        Selenide.$("input[name=AddressTo]").setValue(addressTo);
-        Selenide.$(withText(addressTo)).click();
-        Selenide.$("#Weight").sendKeys(String.valueOf(weight)+Keys.ENTER);
-        Selenide.$("#parcel-summary").find(withText(expectedDeliveryDate));
-    }
 }
